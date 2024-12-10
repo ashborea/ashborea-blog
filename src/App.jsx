161a52f372fff50a389d6bf37d9fb2b518c05826
Marjorie Siad <marjoriesiad@gmail.com>
2024-12-10 20:54:04 +0100

    :construction: create navbar for mobile and desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+      <Navbar />
+    </div>
   )
 }
 
